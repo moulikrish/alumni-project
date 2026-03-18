b75dcc78fb17e659e371cea21557c859f7a089f1
Mouli Krishnan <moulikrish2020@gmail.com>
2026-03-18 21:51:33 +0530

    updated

---
+++ alumini-backend/alumni-portal/src/main/java/com/example/alumni_portal/controller/PingController.java
@@ -0,0 +1,13 @@
+package com.example.alumni_portal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "alive";
+    }
+}
